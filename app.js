f669443ec2a6444d1ccf516308f70008c3566061
Pratik Priyansh <pratikpriyansh5@gmail.com>
2023-11-03 01:34:46 +0530

    Compose Route added

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+dotenv.config();
+const homeStartingContent = "Welcome to Blog, a virtual haven for aspiring writers and passionate bloggers. Unleash your creativity and express your unique voice on our platform. Seamlessly compose and publish your captivating blog posts, sharing your insights, stories, and expertise with the world. Join our community of wordsmiths and let your thoughts paint a masterpiece on this digital canvas of ideas.";
+const aboutContent = "Welcome to Blog, a dynamic platform designed to empower individuals to share their voices and ideas with the world. Whether you're a seasoned writer or a novice storyteller, this is your space to unleash your creativity and craft compelling blog posts.Whether you want to share your knowledge, inspire others, or ignite meaningful discussions, Blog provides the canvas for your thoughts to flourish. Start your blogging journey today and let your creativity soar on Blog.";
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const contactContent = "We would love to hear from you at Blog! If you have any questions, suggestions, or simply want to connect, feel free to reach out. Our team is dedicated to providing you with the best blogging experience.You can email us at pratikpriyansh5@gmail.com";
 
 const app = express();
 
@@ -20,9 +22,9 @@ app.use(express.static("public"));
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb+srv://pratikpriyansh:Pratik2003@gettingstarted.j92gmfu.mongodb.net/blogDB');
+  // await mongoose.connect(`${process.env.MONGO_URL}`);
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
 
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
 const postSchema = new mongoose.Schema({
@@ -33,15 +35,35 @@ const postSchema = new mongoose.Schema({
 const Post = mongoose.model("Post", postSchema);
 
 
-app.get("/", function(req, res){
+app.get("/", async function(req, res){
+  try {
+    const posts = await Post.find({});
+    const formattedPosts = posts.map(function(post) {
+      const contentWithLineBreaks = post.content.replace(/\n/g, "<br>");
+      const contentWithFormatting = contentWithLineBreaks
+        .replace(/%(.*?)%/g, "<strong>$1</strong>")
+        .replace(/\*(.*?)\*/g, "<em>$1</em>");
 
-  Post.find({}).then((posts)=>{
-      res.render("home", {
+      return {
+        title: post.title,
+        content: contentWithFormatting,
+        _id:post._id
+      };
+    });
+
+    res.render("home", {
       startingContent: homeStartingContent,
-      posts: posts
-      });
-  });
+      posts: formattedPosts
+    });
+  } catch (err) {
+    // res.send("nan");
+    res.send("error occured");
+    console.log(err);
+    // Handle the error appropriately
+  }
 });
+
+
 
 app.get("/about", function(req, res){
   res.render("about", {aboutContent: aboutContent});
@@ -60,25 +82,37 @@ app.post("/compose", function(req, res){
     title: req.body.postTitle,
     content: req.body.postBody
   });
-
   Post.insertMany(post);
   posts.push(post);
   res.redirect("/");
 
 });
 
-app.get("/posts/:postId", function(req, res){
-  const requestedPostId = req.params.postId;
+app.get("/posts/:postId", async function(req, res){
+  try {
+    const requestedPostId = req.params.postId;
+    console.log(requestedPostId);
+    const post = await Post.findById(requestedPostId);
+    if (!post) {
+      // Handle the case where the post is not found
+      return res.status(404).render("error", { error: "Post not found" });
+    }
 
-  Post.find({_id: requestedPostId}).then((post)=>{
-    res.render("post",{
-      title:post.title,
-      content:post.content
+    const contentWithLineBreaks = post.content.replace(/\n/g, "<br>");
+    const contentWithFormatting = contentWithLineBreaks
+      .replace(/%(.*?)%/g, "<strong>$1</strong>")
+      .replace(/\*(.*?)\*/g, "<em>$1</em>");
+
+    res.render("post", {
+      title: post.title,
+      content: contentWithFormatting
     });
-  });
-
+  } catch (err) {
+    console.log(err);
+    // Handle the error appropriately
+  }
 });
 
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+  console.log("Server started on port 3000");
 });
